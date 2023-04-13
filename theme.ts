bd047561e0abe4e096638a0464f64aeb23334eec
nguyenthienan <nguyenthienanz2510@gmail.com>
2023-04-14 01:12:57 +0700

    +Config: sanityio theme

---
+++ theme.ts
@@ -0,0 +1,44 @@
+import {buildLegacyTheme, defineConfig} from 'sanity'
+
+const props = {
+  '--my-white': '#fff',
+  '--my-black': '#0f172a',
+  '--my-blue': '#38BDF8',
+  '--my-red': '#cf304a',
+  '--my-yellow': '#ffc107',
+  '--my-green': '#03a66d',
+}
+
+export const myTheme = buildLegacyTheme({
+  /* Base theme colors */
+  '--black': props['--my-black'],
+  '--white': props['--my-white'],
+
+  '--gray': '#94A3B8',
+  '--gray-base': '#94A3B8',
+
+  '--component-bg': props['--my-black'],
+  '--component-text-color': props['--my-white'],
+
+  /* Brand */
+  '--brand-primary': props['--my-blue'],
+
+  // Default button
+  '--default-button-color': '#94A3B8',
+  '--default-button-primary-color': props['--my-blue'],
+  '--default-button-success-color': props['--my-green'],
+  '--default-button-warning-color': props['--my-yellow'],
+  '--default-button-danger-color': props['--my-red'],
+
+  /* State */
+  '--state-info-color': props['--my-blue'],
+  '--state-success-color': props['--my-green'],
+  '--state-warning-color': props['--my-yellow'],
+  '--state-danger-color': props['--my-red'],
+
+  /* Navbar */
+  '--main-navigation-color': props['--my-black'],
+  '--main-navigation-color--inverted': props['--my-white'],
+
+  '--focus-color': props['--my-blue'],
+})
