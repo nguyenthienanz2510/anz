1f57ae2c91e8065666e9b0418bb6e0cc01dd23e8
nguyenthienan <nguyenthienanz2510@gmail.com>
2023-04-20 02:52:16 +0700

    Fix: UI card exp

--- components/Portfolio/WorkExperience/ExperienceCard.tsx
+++ components/Portfolio/WorkExperience/ExperienceCard.tsx
@@ -6,7 +6,7 @@ type Props = {}
 
 export default function ExperienceCard({}: Props) {
   return (
-    <article className='flex w-full flex-shrink-0 snap-center flex-col items-center justify-center gap-5 rounded-lg bg-slate-800 p-5 transition-all hover:bg-slate-700 md:w-[500px] lg:w-[600px]'>
+    <article className='flex w-full flex-shrink-0 snap-center flex-col items-center justify-center gap-3 rounded-lg bg-slate-800 p-5 transition-all hover:bg-slate-700 md:w-[500px] md:gap-4 lg:w-[600px]'>
       <motion.img
         initial={{
           y: -100
@@ -18,52 +18,57 @@ export default function ExperienceCard({}: Props) {
         transition={{
           duration: 1
         }}
-        className='relative mx-auto h-28 w-28 shrink-0 rounded-full object-cover md:h-36 md:w-36'
+        className='relative mx-auto h-24 w-24 shrink-0 rounded-full object-cover 2xl:h-36 2xl:w-36'
         src={
           'https://scontent.fsgn5-11.fna.fbcdn.net/v/t1.6435-9/82192694_2588925624664525_5889774943192743936_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=PF2mKk_PkQIAX-1q7Jw&_nc_ht=scontent.fsgn5-11.fna&oh=00_AfBQkl7cZHG5PGpzI-lU5Ama0cSeAeNnTEp-quCFhIIDaQ&oe=64638F83'
         }
         alt={'avatar'}
       />
-      <div className='space-y-5 text-16 text-color-text-dark-primary dark:text-color-text-light-primary md:text-18'>
-        <h3 className='text-24 font-light md:text-28'>CEO VietnameseHistory</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, sapiente!</p>
+      <div className='space-y-3 text-14 text-color-text-light-primary md:space-y-4 md:text-16 2xl:text-18 2xl:text-18'>
+        <h3 className='text-center text-16 font-light md:text-20 2xl:text-28'>Frontend Developer</h3>
+        <h4 className='text-center text-12 font-bold uppercase tracking-widest md:text-14 2xl:text-18'>Company Name</h4>
+        <p className='font-normal'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, sapiente!</p>
         <div className='flex gap-2'>
           <Image
             src='https://cdn.sanity.io/images/ltuexkre/production/2a67945990f9c2ef568cf7e8483c1a8174556463-500x500.png'
             alt='icon'
             width={40}
             height={40}
-            className='h-10 w-10 rounded-full'
+            className='h-7 w-7 rounded-full md:h-10  md:w-10'
           />
           <Image
             src='https://cdn.sanity.io/images/ltuexkre/production/fa5e6eeb3377a1601e181d0eeb9a60633090cff2-500x500.png'
             alt='icon'
             width={40}
             height={40}
-            className='h-10 w-10 rounded-full'
+            className='h-7 w-7 rounded-full md:h-10  md:w-10'
           />
           <Image
             src='https://cdn.sanity.io/images/ltuexkre/production/2a67945990f9c2ef568cf7e8483c1a8174556463-500x500.png'
             alt='icon'
             width={40}
             height={40}
-            className='h-10 w-10 rounded-full'
+            className='h-7 w-7 rounded-full md:h-10  md:w-10'
           />
           <Image
             src='https://cdn.sanity.io/images/ltuexkre/production/fa5e6eeb3377a1601e181d0eeb9a60633090cff2-500x500.png'
             alt='icon'
             width={40}
             height={40}
-            className='h-10 w-10 rounded-full'
+            className='h-7 w-7 rounded-full md:h-10  md:w-10'
           />
         </div>
-        <p className='text-color-text-dark-secondary dark:text-color-text-light-secondary'>
-          Started work... - Ended work...
-        </p>
-        <ul className='ml-5 list-disc space-y-4'>
+        <p className='text-color-text-light-secondary'>Started work... - Ended work...</p>
+        <ul className='ml-5 list-disc space-y-2 md:space-y-3'>
           <li>Lorem ipsum dolor sit amet.</li>
-          <li>Lorem ipsum dolor sit amet.</li>
-          <li>Lorem ipsum dolor sit amet.</li>
+          <li>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quae ea iste ipsam, unde optio impedit cum
+            incidunt perspiciatis possimus!
+          </li>
+          <li>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quae ea iste ipsam, unde optio impedit cum
+            incidunt perspiciatis possimus!
+          </li>
           <li>Lorem ipsum dolor sit amet.</li>
           <li>Lorem ipsum dolor sit amet.</li>
         </ul>
