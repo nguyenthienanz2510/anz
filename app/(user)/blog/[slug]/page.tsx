83e173cf12f9797512ed15aef00520a2782b2f80
nguyenthienan <nguyenthienanz2510@gmail.com>
2023-04-25 01:35:54 +0700

    +Add: blog detail

---
+++ app/(user)/blog/[slug]/page.tsx
@@ -0,0 +1,86 @@
+import { client } from '@/lib/sanity.client'
+import urlFor from '@/lib/urlFor'
+import { groq } from 'next-sanity'
+import Image from 'next/image'
+import React from 'react'
+
+type Props = {
+  params: {
+    slug: string
+  }
+}
+
+export default async function Post({ params: { slug } }: Props) {
+  const query = groq`
+        *[_type=='post' && slug.current == $slug][0]
+        {
+            ...,
+            author->,
+            categories[]->
+        }
+    `
+
+  const post: Post = await client.fetch(query, { slug })
+
+  console.log(post)
+
+  return (
+    <article>
+      <section className='container'>
+        <div className='relative rounded'>
+          <div className='absolute top-0 -z-10 h-full w-full overflow-hidden rounded'>
+            <div>
+              <Image
+                className='object-cover object-center blur-sm'
+                alt={post.title}
+                title={post.title}
+                src={post.mainImage && urlFor(post.mainImage).url()}
+                fill
+              />
+            </div>
+          </div>
+          <div className='flex flex-col justify-between gap-5 space-y-2 rounded bg-color-primary-light bg-opacity-80 p-5 text-white md:text-16'>
+            <div className='flex flex-col md:flex-row justify-between'>
+              <div className='space-y-2'>
+                <h3 className='text-24 font-bold md:text-36'>{post.title}</h3>
+                <p>
+                  {new Date(post._createdAt).toLocaleDateString('en-US', {
+                    day: 'numeric',
+                    month: 'long',
+                    year: 'numeric'
+                  })}
+                </p>
+              </div>
+              <div className='flex flex-shrink-0 items-start gap-2 pt-2.5'>
+                <Image
+                  className='rounded-full object-cover object-center'
+                  alt={post.author.name}
+                  title={post.author.name}
+                  src={post.mainImage && urlFor(post.author.image).url()}
+                  width={40}
+                  height={40}
+                />
+                <p className='text-18 font-bold leading-[40px]'>{post.author.name}</p>
+              </div>
+            </div>
+            <div className='space-y-5'>
+              <p className='italic'>{post.description}</p>
+              <div className='flex w-full flex-wrap justify-end gap-2 text-12 leading-6 md:text-16 md:leading-8'>
+                {post.categories?.map((category, index) => {
+                  return (
+                    <span
+                      className='inline-block rounded border bg-color-secondary-light px-3 font-normal uppercase tracking-wide text-white md:px-4'
+                      key={index}
+                    >
+                      {category.title}
+                    </span>
+                  )
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </article>
+  )
+}
--- components/Blog/Banner/Banner.tsx
+++ components/Blog/Banner/Banner.tsx
@@ -8,7 +8,7 @@ export default function Banner({}: Props) {
     <section>
       <div className='container flex flex-col justify-between gap-5 py-10 xl:flex-row'>
         <div className='space-y-3 xl:flex-[7]'>
-          <h2 className='text-5xl font-bold leading-[60px] text-color-text-dark-primary transition-colors duration-1000 hover:text-color-primary-light dark:text-color-text-light-primary dark:hover:text-color-primary-dark md:text-7xl md:leading-[84px]'>
+          <h2 className='cursor-wait text-5xl font-bold leading-[60px] text-color-text-dark-primary transition-colors duration-1000 hover:text-color-primary-light dark:text-color-text-light-primary dark:hover:text-color-primary-dark md:text-7xl md:leading-[84px]'>
             Nguyen Thien An&apos;s Daily Blog
           </h2>
           <p className='text-16 uppercase tracking-widest md:text-24'>
